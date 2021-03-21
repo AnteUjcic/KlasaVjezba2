b8de3b3290a46af7ec0c00b692074862aaf7e124
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-03-21 19:17:43 +0100

    Add files via upload

---
+++ NiOPKlaseIObjekti2/Troukut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOPKlaseIObjekti2
+{
+    class Trokut : GemoterijskiLik
+    {
+        public Trokut(int brojStranica = 3) : base(brojStranica)
+        {
+        }
+    }
+}
