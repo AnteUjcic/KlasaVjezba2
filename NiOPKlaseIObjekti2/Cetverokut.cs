59a186e5f283b791b0a76ed49de390763c197e2f
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-03-21 19:17:03 +0100

    Add files via upload

---
+++ NiOPKlaseIObjekti2/Cetverokut.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOPKlaseIObjekti2
+{
+    class Cetverokut: GemoterijskiLik
+    {
+        public Cetverokut(int brojStranica = 4) : base(brojStranica)
+        {
+        }
+
+    }
+}
---
+++ NiOPKlaseIObjekti2/GemoterijskiLik.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiOPKlaseIObjekti2
+{
+    class GemoterijskiLik
+    {
+        int BrojStranica;
+        double[] Stranica = new double[10];
+
+        public GemoterijskiLik(int brojStranica)
+        {
+            BrojStranica = brojStranica;
+        }
+
+        public void UcitajStranice(int BrojStranica)
+        {
+
+
+            BrojStranica = this.BrojStranica;
+            for (int i = 0; i < BrojStranica; i++)
+            {
+                Console.Write("Unesite stranice: ");
+                Stranica[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+        public double[] GetSetBrojStranica { get; set; }
+        public int GetSetStranica { get; set; }
+
+        public double Opseg()
+        {
+
+            double opseg = 0;
+            foreach (var n in Stranica)
+            {
+                opseg += n;
+            }
+            return opseg;
+
+
+
+        }
+    }
+}
